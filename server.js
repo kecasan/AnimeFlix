92c730a77bd8e8b676b8731c84b59539dce0aeb3
Érica Santos <143044036+kecasan@users.noreply.github.com>
2024-11-14 19:47:02 -0300

    Ajustando o server.js

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+
+const express = require('express');
 const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
