83f4df8e5faab45f1939ecfb540b56d066822dec
Érica Santos <143044036+kecasan@users.noreply.github.com>
2024-11-14 20:02:00 -0300

    Interagindo com JS

--- server.js
+++ server.js
@@ -43,3 +43,16 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log('Servidor rodando em http://localhost:${PORT}');
 });
+
+// HTML + JavaScript no frontend
+// Chama a API para exibir a lista de personagens
+fetch('http://localhost:3000/api/pessoa')
+    .then(response => response.json())
+    .then(data => {
+        const list = document.getElementById('character-list');
+        data.forEach(person => {
+            list.innerHTML += `<p>${person.nome} (${person.idade}) - ${person.email}</p>`;
+
+        });
+    })
+.catch(error => console.error('Erro:', error));
